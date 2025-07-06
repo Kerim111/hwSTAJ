691b377c18a09194be712cb323fda66f44213afd
Kerim111 <kerimalp3407@gmail.com>
2025-07-07 02:39:07 +0300

    Update CheckAndUpdate.js

--- client/src/CheckAndUpdate.js
+++ client/src/CheckAndUpdate.js
@@ -6,10 +6,10 @@ import compareDates from './compartedates';
 function CheckAndUpdate(date, setDate, setData, setLastDBping) {
   if (date == null) {
     // First time: load tree, then status
-    axios.get('http://localhost:5000/update')
+    axios.get(`${baseURL}/update`)
       .then(res => {
         setData(res.data);
-        return axios.get('http://localhost:5000/LastUpdate');
+        return axios.get(`${baseURL}/LastUpdate`);
       })
       .then(res => {
         // res.data is now { lastUpdate, LastDBping }
@@ -20,13 +20,13 @@ function CheckAndUpdate(date, setDate, setData, setLastDBping) {
       .catch(err => console.error(err));
   } else {
     // Subsequent polls: only refetch when the DB timestamp changes
-    axios.get('http://localhost:5000/LastUpdate')
+    axios.get(`${baseURL}/LastUpdate`)
       .then(res => {
         const { lastUpdate, LastDBping } = res.data;
         if (compareDates(date, lastUpdate) < 0) {
           setDate(new Date(lastUpdate).toLocaleString());
           setLastDBping(new Date(LastDBping).toLocaleString());
-          return axios.get('http://localhost:5000/update')
+          return axios.get(`${baseURL}/update`)
             .then(r => setData(r.data));
         }
       })
